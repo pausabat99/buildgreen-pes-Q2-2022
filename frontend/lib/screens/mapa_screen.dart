413ae602b63de900a5b4ea3d0064dbb8543975af
Pau Sàbat Martí <pau.sabat.marti@estudiantat.upc.edu>
2022-03-13 19:42:04 +0100

    avances en mapa_screen

--- frontend/lib/screens/mapa_screen.dart
+++ frontend/lib/screens/mapa_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class MapaScreen extends StatefulWidget {
-  const MapaScreen({ Key? key }) : super(key: key);
+  const MapaScreen({Key? key}) : super(key: key);
 
   @override
   State<MapaScreen> createState() => _MapaScreenState();
@@ -10,8 +10,18 @@ class MapaScreen extends StatefulWidget {
 class _MapaScreenState extends State<MapaScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return MaterialApp(
+      title: "Mapa",
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Mapa')),
+        body: Center(
+          child: SizedBox(
+            child: Image.asset('./lib/assets/mapa.png'),
+            height: 500,
+            width: 300,
+          ),
+        ),
+      ),
     );
   }
 }
