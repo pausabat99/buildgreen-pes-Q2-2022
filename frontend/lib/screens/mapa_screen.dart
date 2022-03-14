519b75de3596fb931df2cce28202a4a080b7bf2d
Pau Sàbat Martí <pau.sabat.marti@estudiantat.upc.edu>
2022-03-14 18:40:20 +0100

    cambios mapa_screen

--- frontend/lib/screens/mapa_screen.dart
+++ frontend/lib/screens/mapa_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
 class MapaScreen extends StatefulWidget {
@@ -10,34 +8,42 @@ class MapaScreen extends StatefulWidget {
 }
 
 class _MapaScreenState extends State<MapaScreen> {
+  TextEditingController filterController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Container(
-            alignment: Alignment.center,
-            height: 50,
-            width: 50,
-            child: const Text('Mapa'),
-          ),
-          Container(
-            alignment: Alignment.center,
-            child: const TextField(
-              decoration: InputDecoration(
-                border: UnderlineInputBorder(),
-                labelText: 'Filtrar',
+      body: Container(
+        padding: const EdgeInsets.all(20),
+        child: ListView(
+          children: <Widget>[
+            Container(
+              alignment: Alignment.center,
+              padding: const EdgeInsets.all(10),
+              child: const Text(
+                'Mapa',
+                style: TextStyle(fontSize: 20),
+            )),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                controller: filterController,
+                decoration: const InputDecoration(
+                  border: UnderlineInputBorder(),
+                  labelText: 'Filtrar',
+                ),
               ),
             ),
-          ),
-          Container(
-            alignment: Alignment.center,
-            height: 500,
-            width: 300,
-            color: Colors.blueAccent,
-            child: const Image(image: AssetImage("images/foto_mapa.png"))
-          )
-        ],
+            Container(
+              alignment: Alignment.center,
+              padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+              height: 500,
+              width: 300,
+              color: Colors.blueAccent,
+              child: const Image(image: AssetImage("images/foto_mapa.png"))
+            )
+          ],
+        ),
       ),
     );
   }
