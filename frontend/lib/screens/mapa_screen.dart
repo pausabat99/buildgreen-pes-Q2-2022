49cc902def52e8d271e6882277e97f78b4a2fdac
Pau Sàbat Martí <pau.sabat.marti@estudiantat.upc.edu>
2022-03-16 13:06:44 +0100

    mapa screen terminada, added textfield properly

--- frontend/lib/screens/mapa_screen.dart
+++ frontend/lib/screens/mapa_screen.dart
@@ -12,50 +12,54 @@ class _MapaScreenState extends State<MapaScreen> {
 
   Widget mapaWidget() {
     return Container(
-        width: double.infinity,
-        margin: const EdgeInsets.only(top: 30),
-        child: const Image(image: AssetImage("images/mapa.png")));
+      width: double.infinity,
+      margin: const EdgeInsets.fromLTRB(0, 30, 0, 0),
+      child: const Image(image: AssetImage("images/mapa.png")));
+  }
+
+  Widget filtraWidget() {
+    return Material(
+      // ignore: avoid_unnecessary_containers
+      child: Container(
+        child: TextField(
+          controller: filterController,
+          decoration: const InputDecoration(
+            icon: Icon(Icons.search),
+            hintText: 'Filtrar',
+            border: OutlineInputBorder(),
+          ),
+        )
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        Container(
+    return Material(
+      child: ListView(
+        children: <Widget>[
+          Container(
             alignment: Alignment.topLeft,
-            margin: const EdgeInsets.fromLTRB(25, 40, 0, 0),
+            margin: const EdgeInsets.fromLTRB(50, 40, 0, 0),
             child: const Text(
               'MAPA',
-              style: TextStyle(color: Colors.black, 
-                              fontSize: 40,
-                              decoration: TextDecoration.none),
-              //añadir la FontFamily que sea commún para todos
-            )),
-        Container(
-          margin: const EdgeInsets.fromLTRB(25, 120, 0, 0),
-          height: 30,
-          width: 360,
-          decoration: const BoxDecoration(
-              borderRadius: BorderRadius.only(
-                topRight: Radius.circular(15),
-                topLeft: Radius.circular(15),
-                bottomLeft: Radius.circular(15),
-                bottomRight: Radius.circular(15),
-              ),
-              color: Colors.lightGreen),
-          child: const Text(
-            'Filtrar',
-            textAlign: TextAlign.center,
-            style: TextStyle(color: Colors.white,
-                            fontSize: 20,
-                            decoration: TextDecoration.none),
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 40,
+                  decoration: TextDecoration.none),
+            ),
           ),
-        ),
-        Align(
-          alignment: Alignment.center,
-          child: mapaWidget(),
-        )
-      ],
+          Container(
+            alignment: Alignment.topCenter,
+            margin: const EdgeInsets.fromLTRB(0, 0, 10, 0),
+            padding: const EdgeInsets.all(30),
+            child: filtraWidget(),
+          ),
+          Container(
+            child: mapaWidget(),
+          )
+        ]
+      )
     );
   }
 }
