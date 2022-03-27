91084f6084de95dc7f1522422a98a43b572cca6d
daniel.franco.baena <danifb99@gmail.com>
2022-03-27 18:53:05 +0200

    Pruebas simplificar listapropiedades

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:buildgreen/screens/login_screen.dart';
 import 'package:buildgreen/screens/main_screen.dart';
 import 'package:buildgreen/screens/signup_screen.dart';
+import 'package:buildgreen/screens/area_personal_cliente.dart';
+
 import 'package:flutter/material.dart';
 
 import 'screens/welcome_screen.dart';
 
-void main()  {
+void main() {
   runApp(const MyApp());
 }
 
@@ -19,25 +21,24 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       initialRoute: '/',
       routes: {
-      // When navigating to the "/" route, build the FirstScreen widget.
-      // When navigating to the "/second" route, build the SecondScreen widget.
-      '/login': (context) => const LogInScreen(),
-      '/register': (context) => const SignUpScreen(),
-      '/index': (context) => const MainScreen(),
+        // When navigating to the "/" route, build the FirstScreen widget.
+        // When navigating to the "/second" route, build the SecondScreen widget.
+        '/login': (context) => const LogInScreen(),
+        '/register': (context) => const SignUpScreen(),
+        '/index': (context) => const MainScreen(),
       },
       theme: ThemeData(
         fontFamily: 'Arial',
-        
         textTheme: const TextTheme(
-          headline1: TextStyle(fontSize: 36.0, fontWeight: FontWeight.bold, color: Colors.white),
+          headline1: TextStyle(
+              fontSize: 36.0, fontWeight: FontWeight.bold, color: Colors.white),
           headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
           bodyText2: TextStyle(fontSize: 14.0, color: Colors.black),
           bodyText1: TextStyle(fontSize: 14.0, color: Colors.white),
         ),
-
         primarySwatch: Colors.green,
       ),
-      home: const WelcomeScreen(),
+      home: const AreaPersonalCliente(),
     );
   }
 }
