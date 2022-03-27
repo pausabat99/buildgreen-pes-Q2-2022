01d6a4685f0c0e332d9ab966be683e88ba06af64
Pau Sàbat Martí <pau.sabat.marti@estudiantat.upc.edu>
2022-03-27 23:24:14 +0200

    SIMLISTA: añadido el fondo de pantalla

--- frontend/lib/screens/sim_lista.dart
+++ frontend/lib/screens/sim_lista.dart
@@ -46,170 +46,181 @@ class _SimuladorListState extends State<SimuladorList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SizedBox(
-        height: double.infinity,
-        child: Column(
-          // ignore: prefer_const_literals_to_create_immutables
-          children: <Widget>[
-            SizedBox(
-              height: 100,
-              child: Text('SIMULACIÓN',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40)),
-            ),
-            SizedBox(
-              height: 400,
-              child: SingleChildScrollView(
-                child: ExpansionPanelList(
-                  expansionCallback: (int index, bool isExpanded) {
-                    setState(() {
-                      electrodomesticos[index].isExpanded = !isExpanded;
-                    });
-                  },
-                  children: electrodomesticos.map<ExpansionPanel>((Item item) {
-                    return ExpansionPanel(
-                      headerBuilder: (BuildContext context, bool isExpanded) {
-                        return ListTile(
-                          title: Text(item.headerValue),
-                        );
-                      },
-                      body: Column(
-                        children: <Widget>[
-                          SizedBox(
-                            width: double.infinity,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Align(
-                                    alignment: Alignment.centerLeft,
+      body: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft,
+          colors: [
+            Colors.white,
+            Colors.lightGreen,
+          ],
+        )),
+        child: SizedBox(
+          height: double.infinity,
+          child: Column(
+            // ignore: prefer_const_literals_to_create_immutables
+            children: <Widget>[
+              SizedBox(
+                height: 100,
+                child: Text('SIMULACIÓN',
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40)),
+              ),
+              SizedBox(
+                height: 400,
+                child: SingleChildScrollView(
+                  child: ExpansionPanelList(
+                    expansionCallback: (int index, bool isExpanded) {
+                      setState(() {
+                        electrodomesticos[index].isExpanded = !isExpanded;
+                      });
+                    },
+                    children: electrodomesticos.map<ExpansionPanel>((Item item) {
+                      return ExpansionPanel(
+                        headerBuilder: (BuildContext context, bool isExpanded) {
+                          return ListTile(
+                            title: Text(item.headerValue),
+                          );
+                        },
+                        body: Column(
+                          children: <Widget>[
+                            SizedBox(
+                              width: double.infinity,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Align(
+                                      alignment: Alignment.centerLeft,
+                                      child: Padding(
+                                        padding:
+                                            EdgeInsets.fromLTRB(30, 10, 10, 10),
+                                        child: Text(
+                                          'Selecciona el horario de uso:',
+                                          style: TextStyle(fontSize: 15),
+                                        ),
+                                      )),
+                                  Align(
+                                    alignment: Alignment.centerRight,
                                     child: Padding(
                                       padding:
-                                          EdgeInsets.fromLTRB(30, 10, 10, 10),
-                                      child: Text(
-                                        'Selecciona el horario de uso:',
-                                        style: TextStyle(fontSize: 15),
-                                      ),
-                                    )),
-                                Align(
-                                  alignment: Alignment.centerRight,
-                                  child: Padding(
-                                    padding:
-                                        EdgeInsets.fromLTRB(10, 10, 30, 10),
-                                    child: IconButton(
-                                        onPressed: () => showDialog<String>(
-                                              context: context,
-                                              builder: (BuildContext context) =>
-                                                  AlertDialog(
-                                                title: const Text('¡ATENCIÓN!'),
-                                                content: const Text(
-                                                    '¿Quieres borrar este electrodoméstico?'),
-                                                actions: <Widget>[
-                                                  TextButton(
-                                                    onPressed: () =>
-                                                        Navigator.pop(context,
-                                                            'Cancelar'),
-                                                    child:
-                                                        const Text('Cancelar'),
-                                                  ),
-                                                  TextButton(
-                                                    onPressed: () => {
-                                                      Navigator.pop(
-                                                          context, 'OK'),
-                                                      setState(() {
-                                                        electrodomesticos
-                                                            .removeWhere((Item
-                                                                    currentItem) =>
-                                                                item ==
-                                                                currentItem);
-                                                      })
-                                                    },
-                                                    child: const Text('OK'),
-                                                  ),
-                                                ],
+                                          EdgeInsets.fromLTRB(10, 10, 30, 10),
+                                      child: IconButton(
+                                          onPressed: () => showDialog<String>(
+                                                context: context,
+                                                builder: (BuildContext context) =>
+                                                    AlertDialog(
+                                                  title: const Text('¡ATENCIÓN!'),
+                                                  content: const Text(
+                                                      '¿Quieres borrar este electrodoméstico?'),
+                                                  actions: <Widget>[
+                                                    TextButton(
+                                                      onPressed: () =>
+                                                          Navigator.pop(context,
+                                                              'Cancelar'),
+                                                      child:
+                                                          const Text('Cancelar'),
+                                                    ),
+                                                    TextButton(
+                                                      onPressed: () => {
+                                                        Navigator.pop(
+                                                            context, 'OK'),
+                                                        setState(() {
+                                                          electrodomesticos
+                                                              .removeWhere((Item
+                                                                      currentItem) =>
+                                                                  item ==
+                                                                  currentItem);
+                                                        })
+                                                      },
+                                                      child: const Text('OK'),
+                                                    ),
+                                                  ],
+                                                ),
                                               ),
-                                            ),
-                                        alignment: Alignment.centerRight,
-                                        padding:
-                                            const EdgeInsets.only(right: 20),
-                                        icon: const Icon(Icons.delete)),
-                                  ),
-                                )
-                              ],
+                                          alignment: Alignment.centerRight,
+                                          padding:
+                                              const EdgeInsets.only(right: 20),
+                                          icon: const Icon(Icons.delete)),
+                                    ),
+                                  )
+                                ],
+                              ),
                             ),
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              SizedBox(
-                                  height: 50,
-                                  width: 100,
-                                  child: IconButton(
-                                    icon: Icon(Icons.wb_sunny),
-                                    color: item.activeMorning
-                                        ? Colors.green
-                                        : Colors.black,
-                                    onPressed: () => setState(() {
-                                      item.activeMorning = !item.activeMorning;
-                                    }),
-                                  )),
-                              SizedBox(
-                                  height: 50,
-                                  width: 100,
-                                  child: IconButton(
-                                      icon: Icon(Icons.brightness_4),
-                                      color: item.activeAfternoon
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                SizedBox(
+                                    height: 50,
+                                    width: 100,
+                                    child: IconButton(
+                                      icon: Icon(Icons.wb_sunny),
+                                      color: item.activeMorning
                                           ? Colors.green
                                           : Colors.black,
                                       onPressed: () => setState(() {
-                                            item.activeAfternoon =
-                                                !item.activeAfternoon;
-                                          }))),
-                              SizedBox(
-                                  height: 50,
-                                  width: 100,
-                                  child: IconButton(
-                                      icon: Icon(Icons.brightness_2),
-                                      color: item.activeNight
-                                          ? Colors.green
-                                          : Colors.black,
-                                      onPressed: () => setState(() {
-                                            item.activeNight =
-                                                !item.activeNight;
-                                          })))
-                            ],
-                          )
-                        ],
-                      ),
-                      isExpanded: item.isExpanded,
-                    );
-                  }).toList(),
+                                        item.activeMorning = !item.activeMorning;
+                                      }),
+                                    )),
+                                SizedBox(
+                                    height: 50,
+                                    width: 100,
+                                    child: IconButton(
+                                        icon: Icon(Icons.brightness_4),
+                                        color: item.activeAfternoon
+                                            ? Colors.green
+                                            : Colors.black,
+                                        onPressed: () => setState(() {
+                                              item.activeAfternoon =
+                                                  !item.activeAfternoon;
+                                            }))),
+                                SizedBox(
+                                    height: 50,
+                                    width: 100,
+                                    child: IconButton(
+                                        icon: Icon(Icons.brightness_2),
+                                        color: item.activeNight
+                                            ? Colors.green
+                                            : Colors.black,
+                                        onPressed: () => setState(() {
+                                              item.activeNight =
+                                                  !item.activeNight;
+                                            })))
+                              ],
+                            )
+                          ],
+                        ),
+                        isExpanded: item.isExpanded,
+                      );
+                    }).toList(),
+                  ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 50,
-              child: TextButton(
-                style: ButtonStyle(
-                  foregroundColor:
-                      MaterialStateProperty.all<Color>(Colors.green),
-                  //padding: EdgeInsets.all(10)
+              SizedBox(
+                height: 50,
+                child: TextButton(
+                  style: ButtonStyle(
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(Colors.green),
+                    //padding: EdgeInsets.all(10)
+                  ),
+                  onPressed: () {},
+                  child: Text('Añadir'),
                 ),
-                onPressed: () {},
-                child: Text('Añadir'),
               ),
-            ),
-            SizedBox(
-              height: 50,
-              child: TextButton(
-                style: ButtonStyle(
-                  foregroundColor:
-                      MaterialStateProperty.all<Color>(Colors.green),
-                  //padding: EdgeInsets.all(10)
-                ),
-                onPressed: () {},
-                child: Text('SIMULAR CONSUMO'),
+              SizedBox(
+                height: 50,
+                child: TextButton(
+                  style: ButtonStyle(
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(Colors.green),
+                    //padding: EdgeInsets.all(10)
+                  ),
+                  onPressed: () {},
+                  child: Text('SIMULAR CONSUMO'),
+                )
               )
-            )
-          ],
+            ],
+          ),
         ),
       ),
     );
