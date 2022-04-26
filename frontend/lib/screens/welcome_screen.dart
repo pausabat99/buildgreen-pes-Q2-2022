e9998cdb265c43b3ab326cdcf6a34358b4cd3f26
Pau Sàbat Martí <pau.sabat.marti@estudiantat.upc.edu>
2022-04-26 15:55:35 +0200

    añadido tipo de usuario a shared preferences

--- frontend/lib/screens/welcome_screen.dart
+++ frontend/lib/screens/welcome_screen.dart
@@ -9,113 +9,107 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import "../widgets/general_buttom.dart";
 
-
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 
 class WelcomeScreen extends StatelessWidget {
-
   static const routeName = '/welcome_screen';
 
   const WelcomeScreen({Key? key}) : super(key: key);
 
   Future<void> logInReqAccount(BuildContext context) async {
-    
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    if(prefs.getString("_user_token") != null){
-     EasyLoading.show(status: 'Detected account\nLogging in');
-     final http.Response response = await http.get(
-      Uri.parse('https://buildgreen.herokuapp.com/user/'),
-      headers: <String, String>{
-        HttpHeaders.authorizationHeader: "Token " + prefs.getString("_user_token"),
+
+    if (prefs.getString("_user_token") != null) {
+      EasyLoading.show(status: 'Detected account\nLogging in');
+      final http.Response response = await http.get(
+        Uri.parse('https://buildgreen.herokuapp.com/user/'),
+        headers: <String, String>{
+          HttpHeaders.authorizationHeader:
+              "Token " + prefs.getString("_user_token"),
         },
       );
 
       final responseJson = jsonDecode(response.body);
       EasyLoading.dismiss();
       if (responseJson['user_info'] != null) {
-        Navigator.pushNamedAndRemoveUntil(context, '/index', ((route) => false));
-      }
-      else {
+        Navigator.pushNamedAndRemoveUntil(
+            context, '/index', ((route) => false));
+        prefs.setString("_user_type", responseJson['user_info']['is_admin'].toString());
+      } else {
         await prefs.remove("_user_token");
       }
     }
-  }   
+  }
 
   @override
   Widget build(BuildContext context) {
     logInReqAccount(context);
     double screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: Stack(
-        children: <Widget>[
-          Container(
-            decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topRight,
-              end: Alignment.bottomLeft,
-              colors: [
-                Colors.white,
-                Colors.lightGreen,
-                ],
-              )
-            ),
-          ),
-
-          Column(
-            
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-
-              Expanded(
-                flex: 3,
-                child: Column(
-                  children: [
+        body: Stack(
+      children: <Widget>[
+        Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [
+              Colors.white,
+              Colors.lightGreen,
+            ],
+          )),
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              flex: 3,
+              child: Column(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.only(top: 75, left: 0, right: 0),
+                  ),
+                  Stack(children: <Widget>[
+                    // background profilePic start
                     Container(
-                      padding: const EdgeInsets.only(top: 75, left: 0, right: 0),
-                    ),
-                    Stack(
-                      children: <Widget>[
-                        // background profilePic start
-                        Container(
-                           padding: EdgeInsets.only(top: screenHeight*0.35, left: 50, right: 50),
-
-                          decoration: const BoxDecoration(
-                            image: DecorationImage(
-                              image: AssetImage(
-                                "assets/images/build_green_logo.png",
-                              ),
-                            ),
+                      padding: EdgeInsets.only(
+                          top: screenHeight * 0.35, left: 50, right: 50),
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage(
+                            "assets/images/build_green_logo.png",
                           ),
                         ),
-                        // background profilePic end
-                      ]
+                      ),
                     ),
-                    const Expanded(child: Text(""),),
-                    Container(
-                      padding: const EdgeInsets.fromLTRB(25, 0, 25, 0),
-                      alignment: Alignment.bottomCenter,
-                        child : GeneralButton(
-                          title: 'Entrar', 
-                          textColor: Colors.white , 
-                          action: () => {Navigator.pushNamed(context, '/login')}
-                        ),
-                    ),
-                    Container(
-                      padding: const EdgeInsets.fromLTRB(25, 25, 25, 100),
-                      alignment: Alignment.bottomCenter,
-                      child : GeneralButton(
-                          
-                          title: 'Registrarse', 
-                          textColor: Colors.white , 
-                          action: () => {Navigator.pushNamed(context, '/register')}
-                        ),
-                    ),
-                  ],
-                ),
-              ),  
-            ],
-          ),
-        ],)
-    );
+                    // background profilePic end
+                  ]),
+                  const Expanded(
+                    child: Text(""),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(25, 0, 25, 0),
+                    alignment: Alignment.bottomCenter,
+                    child: GeneralButton(
+                        title: 'Entrar',
+                        textColor: Colors.white,
+                        action: () => {Navigator.pushNamed(context, '/login')}),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(25, 25, 25, 100),
+                    alignment: Alignment.bottomCenter,
+                    child: GeneralButton(
+                        title: 'Registrarse',
+                        textColor: Colors.white,
+                        action: () =>
+                            {Navigator.pushNamed(context, '/register')}),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ],
+    ));
   }
 }
