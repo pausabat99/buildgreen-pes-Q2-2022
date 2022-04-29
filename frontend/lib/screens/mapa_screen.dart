d2a399c719688a6c8638c45759d1c4ec639805b5
MikoMikarro <lopezcuestam@gmail.com>
2022-04-29 13:18:53 +0200

    starting places

--- frontend/lib/screens/mapa_screen.dart
+++ frontend/lib/screens/mapa_screen.dart
@@ -7,7 +7,7 @@ import 'package:buildgreen/widgets/expandable_action_button.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter_heatmap/google_maps_flutter_heatmap.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-
+import 'package:buildgreen/classes/place_model.dart';
 class MapaScreen extends StatefulWidget {
   const MapaScreen({Key? key}) : super(key: key);
 
@@ -19,21 +19,41 @@ class _MapaScreenState extends State<MapaScreen> {
   final Completer<GoogleMapController> _controller = Completer();
   final _lController = RequestPermissionController();
 
+  List<Marker> allMarkers = [];
+
+
   static const CameraPosition _kBarcelona = CameraPosition(
     target: LatLng(41.4026556, 2.1587003),
-    zoom: 14.4746,
+    zoom: 15,
+    bearing: 45,
+    tilt: 45,
   );
 
   final Set<Heatmap> _heatmaps = {};
 
-  final LatLng _heatmapLocation = const LatLng(37.42796133580664, -122.085749655962);
-
   TextEditingController filterController = TextEditingController();
-  
 
   @override
   void dispose(){
     super.dispose();
+  }
+
+  @override
+  void initState(){
+    super.initState();
+    for (var element in placeList) {
+      allMarkers.add(
+        Marker(
+          markerId: MarkerId(element.placeName),
+          draggable: false,
+          infoWindow: InfoWindow(
+            title: element.placeName,
+            snippet: element.placeAddress
+          ),
+          position: element.locationCords
+        )
+      );
+    }
   }
   @override
   Widget build(BuildContext context) {
@@ -100,11 +120,6 @@ class _MapaScreenState extends State<MapaScreen> {
           ),
         ],
       ),
-      /*floatingActionButton: FloatingActionButton.extended(
-        onPressed: _addHeatmap,
-        label: const Text('Add Heatmap'),
-        icon: const Icon(Icons.add_box),
-      ),*/
       resizeToAvoidBottomInset: false,
       backgroundColor: Colors.transparent,
       body: SafeArea(
@@ -144,6 +159,9 @@ class _MapaScreenState extends State<MapaScreen> {
                     myLocationButtonEnabled: true,
                     myLocationEnabled: true,
                     zoomGesturesEnabled: true,
+                    markers: Set.from(allMarkers),
+                    rotateGesturesEnabled: false,
+
                     onMapCreated: (GoogleMapController controller) {
                       _controller.complete(controller);
                     },
@@ -156,33 +174,5 @@ class _MapaScreenState extends State<MapaScreen> {
         ),
       ),
     );
-  }
-
-  void _addHeatmap(){
-    setState(() {
-      _heatmaps.add(
-        Heatmap(
-          heatmapId: HeatmapId(_heatmapLocation.toString()),
-          points: _createPoints(_heatmapLocation),
-          radius: 20,
-          visible: true,
-          gradient:  HeatmapGradient(
-            colors: const <Color>[Colors.green, Colors.red], startPoints: const <double>[0.2, 0.8]
-          )
-        )
-      );
-    });
-  }
-
-  List<WeightedLatLng> _createPoints(LatLng location) {
-    final List<WeightedLatLng> points = <WeightedLatLng>[];
-    //Can create multiple points here
-    points.add(_createWeightedLatLng(location.latitude,location.longitude, 0));
-    points.add(_createWeightedLatLng(location.latitude-1,location.longitude, 1)); 
-    return points;
-  }
-
-  WeightedLatLng _createWeightedLatLng(double lat, double lng, int weight) {
-    return WeightedLatLng(point: LatLng(lat, lng), intensity: weight);
   }
 }
