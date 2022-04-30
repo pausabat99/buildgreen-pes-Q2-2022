2e95c9ed73767523b7d2c5ce2c3395b2437dfc1c
daniel.franco.baena <danifb99@gmail.com>
2022-04-30 20:18:12 +0200

    Nueva verison AreaPersonal

--- frontend/lib/screens/area_personal_cliente.dart
+++ frontend/lib/screens/area_personal_cliente.dart
@@ -64,216 +64,172 @@ class _AreaPersonalCliente extends State<AreaPersonalCliente> {
     );
   }
 
+  Widget createbox() {
+    return Flexible(
+      child: Container(
+        alignment: Alignment.topLeft,
+        padding: const EdgeInsets.all(10.0),
+        child: const Align(
+          alignment: Alignment.topRight,
+          child: Icon(Icons.arrow_forward_ios,
+              color: Color.fromARGB(255, 94, 95, 94)),
+        ),
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(255, 255, 255, 255),
+          borderRadius: BorderRadius.circular(12),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.transparent,
       body: ListView(
         children: [
-          Column(
+          Container(
+            alignment: Alignment.topLeft,
+            padding: const EdgeInsets.only(
+              left: 50,
+              top: 10,
+            ),
+            child: Text(
+              AppLocalizations.of(context)!.areapersonal,
+              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
             children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.only(
-                      left: 50,
-                    ),
-                    child: Text(
-                      AppLocalizations.of(context)!.areapersonal,
-                      style: const TextStyle(
-                          fontWeight: FontWeight.bold, fontSize: 40),
-                    ),
-                  ),
-                ],
+              const Padding(
+                padding: EdgeInsets.only(
+                  left: 50,
+                  top: 100,
+                ),
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: const <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(
-                      left: 50,
-                    ),
-                    child: Text(
-                      'Carrer de Rosello, 1\n08029 Barcelona',
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                    ),
-                  ),
-                ],
+              createbox(),
+              const Padding(
+                padding: EdgeInsets.only(
+                  left: 10,
+                ),
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  GestureDetector(
-                    onTap: () {},
-                    child: Container(
-                      width: 190.0,
-                      height: 190.0,
-                      margin: const EdgeInsets.all(50.0),
-                      padding: const EdgeInsets.all(10.0),
-                      child: const Align(
-                        alignment: Alignment.topRight,
-                        child: Icon(Icons.arrow_forward_ios,
-                            color: Color.fromARGB(255, 94, 95, 94)),
-                      ),
-                      decoration: BoxDecoration(
-                        color: const Color.fromARGB(255, 255, 255, 255),
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    width: 190.0,
-                    height: 190.0,
-                    margin: const EdgeInsets.all(50.0),
-                    padding: const EdgeInsets.all(10.0),
-                    child: const Align(
-                      alignment: Alignment.topRight,
-                      child: Icon(Icons.arrow_forward_ios,
-                          color: Color.fromARGB(255, 94, 95, 94)),
-                    ),
-                    decoration: BoxDecoration(
-                      color: const Color.fromARGB(255, 255, 255, 255),
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                  ),
-                ],
+              createbox(),
+              const Padding(
+                padding: EdgeInsets.only(
+                  right: 50,
+                ),
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.only(
-                      left: 50,
-                    ),
-                    child: Text(
-                      AppLocalizations.of(context)!.preciosatiemporeal,
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  Container(
-                    height: 70.0,
-                    margin: const EdgeInsets.all(50.0),
-                    padding: const EdgeInsets.all(10.0),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        const Flexible(
-                          flex: 1,
-                          child: Align(
-                            alignment: Alignment.centerLeft,
-                            child: Image(
-                              image: AssetImage("assets/images/euro.png"),
-                              height: 50,
-                              width: 50,
-                            ),
-                          ),
-                        ),
-                        Flexible(
-                          flex: 10,
-                          child: Align(
-                            alignment: Alignment.centerLeft,
-                            child: Text(
-                              AppLocalizations.of(context)!
-                                  .verpreciosatiemporeal,
-                              style: const TextStyle(fontSize: 20),
-                            ),
-                          ),
-                        ),
-                        const Flexible(
-                          flex: 3,
-                          child: Align(
-                            alignment: Alignment.centerRight,
-                            child: Icon(Icons.arrow_forward_ios,
-                                color: Color.fromARGB(255, 94, 95, 94)),
-                          ),
-                        )
-                      ],
-                    ),
-                    decoration: BoxDecoration(
-                      color: const Color.fromARGB(255, 255, 255, 255),
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.only(
-                        left: 50,
-                      ),
-                      child: Text(
-                        AppLocalizations.of(context)!.consumoenergetico,
-                        style: const TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 20),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  Expanded(
-                    child: Container(
-                      height: 400.0,
-                      margin: const EdgeInsets.all(50.0),
-                      padding: const EdgeInsets.all(10.0),
-                      child: const Image(
-                        fit: BoxFit.fill,
-                        image: AssetImage(
-                            "assets/images/cual_es_el_gasto_en_electricidad2.png"),
-                      ),
-                      decoration: BoxDecoration(
-                        color: const Color.fromARGB(255, 255, 255, 255),
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: const <Widget>[
-                    Padding(
-                      padding: EdgeInsets.only(
-                        bottom: 50,
-                      ), // use Spacer
-                    ),
-                  ]),
             ],
           ),
-          DropdownButton<Locale>(
-              value: lang,
-              onChanged: (Locale? val) {
-                MyApp.of(context)?.setLocale(val!);
-                lang = val!;
-              },
-              items: const [
-                Locale('es', 'ES'),
-                Locale('ca', 'CAT'),
-              ]
-                  .map((e) => DropdownMenuItem(
-                        value: e,
-                        child: _title(e.languageCode),
-                      ))
-                  .toList()),
-          GeneralButton(
-            title: "Log out",
-            action: onPressedLogOut,
-            textColor: Colors.black,
+          Container(
+            alignment: Alignment.centerLeft,
+            padding: const EdgeInsets.only(
+              left: 50,
+              top: 10,
+            ),
+            child: Text(
+              AppLocalizations.of(context)!.preciosatiemporeal,
+              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
           ),
-          const Padding(padding: EdgeInsets.only(top: 20))
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Flexible(
+                child: Container(
+                  margin: const EdgeInsets.only(
+                    left: 50,
+                    top: 40,
+                    right: 50,
+                  ),
+                  padding: const EdgeInsets.all(10.0),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              AppLocalizations.of(context)!
+                                  .verpreciosatiemporeal,
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 18),
+                            ),
+                          ],
+                        ),
+                      ),
+                      const Image(
+                        image: AssetImage("assets/images/euro.png"),
+                        height: 50,
+                        width: 50,
+                      ),
+                      const Icon(Icons.arrow_forward_ios,
+                          color: Color.fromARGB(255, 94, 95, 94)),
+                    ],
+                  ),
+                  decoration: BoxDecoration(
+                    color: const Color.fromARGB(255, 255, 255, 255),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Container(
+            alignment: Alignment.centerLeft,
+            padding: const EdgeInsets.only(
+              left: 50,
+              top: 40,
+            ),
+            child: Text(
+              AppLocalizations.of(context)!.consumoenergetico,
+              style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+            ),
+          ),
+          Container(
+            height: 200.0,
+            margin: const EdgeInsets.only(
+              left: 50,
+              top: 40,
+              right: 50,
+            ),
+            padding: const EdgeInsets.all(10.0),
+            child: const Image(
+              fit: BoxFit.fill,
+              image: AssetImage(
+                  "assets/images/cual_es_el_gasto_en_electricidad2.png"),
+            ),
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 255, 255, 255),
+              borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.only(
+              left: 20,
+              top: 40,
+              right: 20,
+            ),
+            child: GeneralButton(
+              title: "Acceder al Perfil",
+              action: onPressedLogOut,
+              textColor: Colors.black,
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.only(
+              left: 20,
+              top: 20,
+              right: 20,
+              bottom: 20,
+            ),
+            child: GeneralButton(
+              title: "Cerrar Session",
+              action: onPressedLogOut,
+              textColor: Colors.black,
+            ),
+          ),
         ],
       ),
     );
